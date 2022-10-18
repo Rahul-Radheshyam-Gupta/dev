c34489ac9163e502000d81689cd97273f3287338
Rahul Gupta <developer3@teamnest.com>
2022-10-18 18:04:16 +0530

    Final changes related to admin dashboard and text changes

--- core/templatetags/custom_template_filter.py
+++ core/templatetags/custom_template_filter.py
@@ -1,6 +1,7 @@
 from django.utils.safestring import mark_safe
 from django.template import Library
 import json
+from datetime import date,datetime
 register = Library()
 
 
@@ -11,3 +12,8 @@ def js(obj):
 @register.filter()
 def sub(a, b):
     return a-b
+
+@register.filter()
+def date(d):
+    dt = datetime.strptime(d[:10], '%Y-%m-%d').strftime('%d %b %Y')
+    return dt
